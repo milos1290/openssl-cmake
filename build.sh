e8e0ab9aeaa23107c75245b6f7cff2eeeb08ae23
milos1290 <milos1290@gmail.com>
2021-03-04 14:55:48 +0100

    improvements
    * x86 support
    * min api to 21
    * release build by default

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-android_archs="armeabi-v7a arm64-v8a x86_64"
+android_archs="armeabi-v7a arm64-v8a x86_64 x86"
+android_api_level=21
 
 rm -rf build
 rm -rf prebuilt
@@ -9,7 +10,13 @@ do
     mkdir -p ${build_dir}
     cd ${build_dir}
 
-    cmake ../../ -DCMAKE_TOOLCHAIN_FILE="${ANDROID_NDK_ROOT}/build/cmake/android.toolchain.cmake" -DANDROID_ABI=${arch}
+    cmake ../../ \
+        -DCMAKE_TOOLCHAIN_FILE="${ANDROID_NDK_ROOT}/build/cmake/android.toolchain.cmake" \
+        -DANDROID_ABI=${arch} \
+        -DANDROID_PLATFORM=${android_api_level} \
+        -DCMAKE_BUILD_TYPE=RELEASE \
+        -DCMAKE_SYSTEM_NAME=Android \
+        -DCMAKE_SYSTEM_VERSION=${android_api_level}
     make install -j12
 
     cd -
